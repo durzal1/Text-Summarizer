c5319bde4c66d9aae00df4e8cb4c241bcc5d6a13
durzal1 <67489054+durzal1@users.noreply.github.com>
2023-08-01 15:59:17 -0400

    code

--- dataset.py
+++ dataset.py
@@ -1,3 +1,4 @@
+import nltk
 import torch
 from torch.utils.data import Dataset
 from torchtext.data.utils import get_tokenizer
@@ -10,12 +11,14 @@ import string
 stop_words = set(stopwords.words('english'))
 
 class CustomDataset(Dataset):
-    def __init__(self, text_data, labels, paragraph_size):
+    def __init__(self, text_data, labels, paragraph_size, review_size):
+        self.combined =  labels
         self.text_data = text_data
         self.labels = labels
         self.tokenizer = get_tokenizer('basic_english')
-        self.vocab = build_vocab_from_iterator(self.tokenizer(text) for text in text_data)
+        self.vocab = build_vocab_from_iterator(self.tokenizer(text) for text in self.combined)
         self.paragraph_size = paragraph_size
+        self.review_size = review_size
 
     def __len__(self):
         return len(self.text_data)
@@ -42,37 +45,71 @@ class CustomDataset(Dataset):
 
         return cleaned_text
 
-
     def __getitem__(self, idx):
-
         text = self.text_data[idx]
         label = self.labels[idx]
 
-        if label == "positive":
-            label = 1
-        else:
-            label = 0
+        # Clean up the text
+        text2 = self.clean_text(text)
 
-        # clean up the text
-        text = self.clean_text(text)
+        # Tokenize text and convert to numerical
+        tokens = self.tokenizer(text2)
 
-        features = np.zeros(self.reviews_size, dtype=int)
-
-        # tokenize text and convert to numerical
-        tokens = self.tokenizer(text)
-
-        # create the numerical representation
+        # Create the numerical representation
         numerical_representation = []
-
         for token in tokens:
             if token not in self.vocab:
                 continue
             numerical_representation.append(self.vocab[token])
 
+        # Init SOS and EOS tokens
+        SOS = len(self.vocab) + 1
+        EOS = len(self.vocab) + 2
+
+        # Add SOS token as the first value
+        numerical_representation = [SOS] + numerical_representation
+
+        # If numerical_representation is longer than self.paragraph_size, truncate it and add EOS token
+        if len(numerical_representation) >= self.paragraph_size:
+            numerical_representation = numerical_representation[:self.paragraph_size - 1]  # Make space for EOS
+            numerical_representation.append(EOS)
+        else:
+            # Calculate the number of paddings required
+            num_paddings = self.paragraph_size - len(numerical_representation) -1
+
+            # Append EOS token if there is still space for it
+            numerical_representation.append(EOS)
+
+            # Pad the numerical_representation
+            padding = [0] * num_paddings
+            numerical_representation += padding
+
         numerical_representation = torch.tensor(numerical_representation)
 
-        # updates features with the values of numerical representations
-        # adds padding if there is extra room and cuts if words is over self.reviews_size
-        features[:len(numerical_representation)] = numerical_representation[:self.reviews_size]
+        ## Convert Label to tensor with numerical representation
 
-        return features, label
+        # Tokenize label and convert to numerical representation
+        tokens_label = self.tokenizer(label)
+        numerical_representation_label = [self.vocab[token] for token in tokens_label if token in self.vocab]
+
+        # Add SOS token as the first value in label
+        numerical_representation_label = [SOS] + numerical_representation_label
+
+        # If numerical_representation_label is longer than self.review_size, truncate it and add EOS token
+        if len(numerical_representation_label) >= self.review_size:
+            numerical_representation_label = numerical_representation_label[:self.review_size - 1]  # Make space for EOS
+            numerical_representation_label.append(EOS)
+        else:
+            # Calculate the number of paddings required
+            num_paddings_label = self.review_size - len(numerical_representation_label) - 1
+
+            # Append EOS token if there is still space for it
+            numerical_representation_label.append(EOS)
+
+            # Pad the numerical_representation_label
+            padding_label = [0] * num_paddings_label
+            numerical_representation_label += padding_label
+
+        numerical_representation_label = torch.tensor(numerical_representation_label)
+
+        return numerical_representation, numerical_representation_label
--- main.py
+++ main.py
@@ -7,19 +7,23 @@ from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 from torchdata.datapipes.iter import IterableWrapper, FileOpener
 from dataset import *
+import nltk
+from tqdm import tqdm
+
 
 # Constants
 BATCH_SIZE = 64
 NUM_EPOCHS = 10
 LEARNING_RATE = .001
 DEVICE = "cuda" if torch.cuda.is_available else "cpu"
+DEVICE = "cpu"
 embedding_dim = 100
 hidden_dim = 100
 NUM_LAYERS = 2
 CLIP = 5
 
 # Retrieve data
-datapipe = IterableWrapper(["IMDB Dataset.csv"])
+datapipe = IterableWrapper(["test.csv"])
 datapipe = FileOpener(datapipe, mode='b')
 datapipe = datapipe.parse_csv(skip_lines=1)
 
@@ -31,8 +35,8 @@ for sample in datapipe:
     cnt += 1
 
     # adds the data and separates it
-    paragraph_data.append(sample[0])
-    summarized_data.append(sample[1])
+    paragraph_data.append(sample[1])
+    summarized_data.append(sample[2])
 
     # cnt here is used to ensure that it perfectly fits with my batch_size
     # if (cnt == 49984):
@@ -46,13 +50,19 @@ x_train, x_test, y_train, y_test = train_test_split(paragraph_data, summarized_d
 
 
 # Combine the X and Y and adjust it before converting to a tensor
-train_data = CustomDataset(x_train, y_train,)
-test_data = CustomDataset(x_test, y_test)
+train_data = CustomDataset(x_train, y_train, 500, 50)
+test_data = CustomDataset(x_test, y_test, 500, 50)
 
+train_data.__getitem__(0)
 
 # create tensor dataset
 train_loader = DataLoader(train_data, shuffle=True, batch_size=BATCH_SIZE)
 test_loader = DataLoader(test_data, shuffle=True, batch_size=BATCH_SIZE)
+
+# Important variables (Add 2 because of SOS and EOS tokens)
+input_vocab_size = len(train_data.vocab) + 3
+output_vocab_size = len(train_data.vocab) + 3
+
 
 
 class Encoder(nn.Module):
@@ -63,15 +73,16 @@ class Encoder(nn.Module):
         self.embedding = nn.Embedding(input_vocab_size, hidden_size)
         self.rnn = nn.LSTM(hidden_size, hidden_size, num_layers=num_layers)
 
-    def forward(self, input_seq):
+    def forward(self, input_seq, hidden):
         embedded = self.embedding(input_seq)
-        output, hidden = self.rnn(embedded)
+        output, hidden = self.rnn(embedded, hidden)
         return output, hidden
 
 # Step 4: Define the Decoder class
 class Decoder(nn.Module):
     def __init__(self, output_vocab_size, hidden_size, num_layers=1):
         super(Decoder, self).__init__()
+        self.output_vocab_size = output_vocab_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.embedding = nn.Embedding(output_vocab_size, hidden_size)
@@ -79,6 +90,7 @@ class Decoder(nn.Module):
         self.output_layer = nn.Linear(hidden_size, output_vocab_size)
 
     def forward(self, input_seq, hidden):
+
         embedded = self.embedding(input_seq)
         output, hidden = self.rnn(embedded, hidden)
         output = self.output_layer(output)
@@ -90,35 +102,98 @@ class Decoder(nn.Module):
 class Seq2Seq(nn.Module):
     def __init__(self, input_vocab_size, output_vocab_size, hidden_size, num_layers=1):
         super(Seq2Seq, self).__init__()
+        self.hidden_size = hidden_size
         self.encoder = Encoder(input_vocab_size, hidden_size, num_layers=num_layers)
         self.decoder = Decoder(output_vocab_size, hidden_size, num_layers=num_layers)
-
+    """
+    NOTE: Every tensor in the batch is being computed at the same time
+    """
     def forward(self, input_seq, target_seq):
-        encoder_output, encoder_hidden = self.encoder(input_seq)
 
-        # Initialize the decoder's hidden state with the encoder's final hidden state
-        decoder_hidden = encoder_hidden
+        # Shape should be (paragraph_size, BATCH_SIZE)
+        input_seq = input_seq.transpose(0, 1)
+        target_seq = target_seq.transpose(0, 1)
+
+        # Initialize the hidden layer
+        h0 = torch.zeros((self.decoder.num_layers, BATCH_SIZE, self.hidden_size)).to(DEVICE)
+        c0 = torch.zeros((self.decoder.num_layers, BATCH_SIZE, self.hidden_size)).to(DEVICE)
+        hidden = (h0, c0)
+
+        encoder_output, encoder_hidden = self.encoder(input_seq, hidden)
 
         # Get the maximum sequence length for the target sequence
         max_len = target_seq.size(0)
 
-        batch_size = target_seq.size(1)  # Get the batch size
-
         # Initialize the output tensor to store the generated sequence
-        decoder_outputs = torch.zeros(max_len, batch_size, self.decoder.output_vocab_size).to(target_seq.device)
+        decoder_outputs = torch.zeros(max_len, BATCH_SIZE, self.decoder.output_vocab_size).to(input_seq.device)
 
         # Start the decoding process with the start-of-sequence token as the first input
-        decoder_input = target_seq[0, :]  # Use the start-of-sequence token from the target sequence
+        decoder_input = target_seq[0, :].unsqueeze(0)       # Use the start-of-sequence token from the target sequence
+
 
         # Iterate over the target sequence and generate the output
         for t in range(1, max_len):
-            decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden)
+            decoder_output, encoder_hidden = self.decoder(decoder_input, encoder_hidden)
             decoder_outputs[t] = decoder_output
 
             # Use teacher forcing: set the next decoder input to be the true target token at the current time step
-            decoder_input = target_seq[t, :]
+            decoder_input = target_seq[t, :].unsqueeze(0)
 
         return decoder_outputs
 
 
 # Training
+
+# Initialize the model, loss function, and optimizer
+model = Seq2Seq(input_vocab_size, output_vocab_size, hidden_dim, num_layers=NUM_LAYERS)
+model.to(DEVICE)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+# Set the model to training mode
+model.train()
+
+# Training loop
+for epoch in range(NUM_EPOCHS):
+    total_loss = 0
+
+    loop = tqdm(train_loader, leave=True)
+
+    for batch, (x,y) in enumerate(loop):
+        # Transfer the data to the device (GPU if available)
+        x = x.to(DEVICE)
+        y = y.to(DEVICE)
+
+        # Forward pass
+        model.zero_grad()
+
+        output = model(x,y)
+
+        # Reshape the output and target for computing loss
+        output = output.view(-1, output_vocab_size)
+        target = y.view(-1)
+
+        # Compute the loss
+        loss = criterion(output, target)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+
+        # help prevent exploding gradient problem
+        nn.utils.clip_grad_norm_(model.parameters(), CLIP)
+
+        # update gradients
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    # Calculate the average loss for the epoch
+    average_loss = total_loss / len(train_loader)
+
+    # Print the progress
+    print(f"Epoch [{epoch+1}/{NUM_EPOCHS}], Loss: {average_loss:.4f}")
+
+# Save the trained model if desired
+# torch.save(model.state_dict(), "text_summarizer_model.pth")
